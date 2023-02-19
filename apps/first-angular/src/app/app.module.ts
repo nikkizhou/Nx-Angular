1bd35d40982afd474f6b5b9d700aaf049f08bd5b
unknown <nikkizhou99@gmail.com>
2023-02-19 23:13:28 +0100

    added router

--- apps/first-angular/src/app/app.module.ts
+++ apps/first-angular/src/app/app.module.ts
@@ -4,18 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule,Routes } from '@angular/router';
-import { appRoutes } from './app.routes';
+import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-const addRoute: Routes = [
-  {path:'',component:TasksComponent}
-]
+const appRoutes: Routes = [
+  { path: '', component: TasksComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   declarations: [
@@ -25,13 +27,18 @@ const addRoute: Routes = [
     TasksComponent,
     TaskItemComponent,
     AddTaskComponent,
+    AboutComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking',enableTracing:true }),
+    RouterModule.forRoot(appRoutes, {
+      initialNavigation: 'enabledBlocking',
+      enableTracing: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- apps/first-angular/src/app/components/header/header.component.ts
+++ apps/first-angular/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import {Subscription} from 'rxjs'
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-header',
@@ -13,7 +14,7 @@ export class HeaderComponent {
   showAddTask: boolean = true;
   subscription!: Subscription
   
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router:Router) {
     // the value here is the showAddTask variable value from uiService
     this.subscription = this.uiService
       .onToggle()
@@ -22,6 +23,10 @@ export class HeaderComponent {
 
   toggleAddTask() {
     this.uiService.toggleAddTask()
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route
   }
 
 }
