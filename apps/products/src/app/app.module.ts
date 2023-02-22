ead02d01b4a4c4aa79f22abeda8384cb58ae13d2
unknown <nikkizhou99@gmail.com>
2023-02-22 21:14:16 +0100

    done with product-list component

--- apps/products/src/app/app.module.ts
+++ apps/products/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { WelcomeComponent } from './components/welcom/welcome.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './pipes/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent],
+    ProductListComponent,
+    ConvertToSpacesPipe],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
--- apps/products/src/app/components/product-list/product-list.component.ts
+++ apps/products/src/app/components/product-list/product-list.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import data from '../../../api/products/products.json'
+import { IProduct } from '../../interfaces/product';
 
 @Component({
   selector: 'app-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
-  pageTitle: string = 'Product List';
-  products: any[] = data
+export class ProductListComponent implements OnInit{
+  pageTitle = 'Product List';
+  products: IProduct[] = data
+  showImage = true
+  private _listFilter = ''
+  filteredProducts : IProduct[] = []
 
+  get listFilter():string {
+    return this._listFilter
+  }
+  set listFilter(value:string){
+    this._listFilter = value
+    this.filteredProducts = this.performFilter(value)
+  }
+
+  performFilter(filterBy: string):IProduct[] {
+    return this.products
+      .filter((p: IProduct) =>
+      p.productName.toLowerCase()
+      .includes(filterBy.toLowerCase())
+    )
+  }
+
+  toggleImage() {
+    this.showImage = !this.showImage
+  }
+
+  ngOnInit(): void{
+    this.listFilter='cart'
+  }
 }
